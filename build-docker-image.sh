f784dd720d3a9715e10928c6a966d1665e62f7d8
Samuli Heljo <samuli@samuliheljo.com>
2016-02-25 13:45:37 +0200

    Improve docker build

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,11 +5,10 @@
 #DOCKER_EMAIL=
 #DOCKER_USER=
 #DOCKER_AUTH=
-#FONTSTACK_PASSWORD=
 
 # Build image
 IMAGE="hsldevcom/hsl-map-server"
-docker build --build-arg FONTSTACK_PASSWORD=$FONTSTACK_PASSWORD --tag="$IMAGE:$DOCKER_TAG" .
+docker build --build-arg --tag="$IMAGE:$DOCKER_TAG" .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $IMAGE:$DOCKER_TAG
 docker tag $IMAGE:$DOCKER_TAG $IMAGE:latest
