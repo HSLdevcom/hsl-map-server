ace90fa00c6c7b3f2a5428224059c0541c0d9b73
Hannes Junnila <hannes.junnila@gmail.com>
2017-01-24 11:02:48 +0200

    Adjust to changes in hsl-map-style
    Add swedish map tiles

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     "source": {
       "protocol": "gl:",
       "query": {},
-      "style": require("hsl-map-style/hsl-gl-map-v9-no-icons.json")
+      "style": require("hsl-map-style/").generateStyle({})
     },
     "headers": {
       "Cache-Control": "public,max-age=604800"
@@ -19,7 +19,27 @@ module.exports = {
     "source": {
       "protocol": "gl:",
       "query": {layerTileSize: 256},
-      "style": require("hsl-map-style")
+      "style": require("hsl-map-style/").generateStyle({extensions: "icons"})
+    },
+    "headers": {
+      "Cache-Control": "public,max-age=604800"
+    }
+  },
+  "/hsl-map-sv": {
+    "source": {
+      "protocol": "gl:",
+      "query": {},
+      "style": require("hsl-map-style/").generateStyle({lang: "sv"})
+    },
+    "headers": {
+      "Cache-Control": "public,max-age=604800"
+    }
+  },
+  "/hsl-map-sv-256": {
+    "source": {
+      "protocol": "gl:",
+      "query": {layerTileSize: 256},
+      "style": require("hsl-map-style/").generateStyle({extensions: "icons", lang: "sv"})
     },
     "headers": {
       "Cache-Control": "public,max-age=604800"
