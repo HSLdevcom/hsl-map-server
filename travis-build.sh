e3989ec402597232731c1963432449e4bbac45ab
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-08-03 18:50:52 +0300

    Add tests to travis build

--- travis-build.sh
+++ travis-build.sh
@@ -10,9 +10,44 @@ DOCKER_TAG=${TRAVIS_BUILD_ID:-latest}
 DOCKER_IMAGE=$ORG/hsl-map-server:${DOCKER_TAG}
 DOCKER_IMAGE_LATEST=$ORG/hsl-map-server:latest
 
-echo Building hsl-map-server: $DOCKER_IMAGE
+function test {
+  URL=$1
+  MINLENGTH=$2
 
-docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
+  echo Testing $URL
+
+  HEADERS=$(curl -sI $URL)
+
+  if [ -z "$HEADERS" ] ; then
+    echo No response
+    exit 1
+  fi
+
+  STATUS=$(head -1 <<< "$HEADERS")
+  if ! grep -q "200 OK" <<< "$HEADERS" ; then
+    echo Unexpected status code: $STATUS
+    exit 1
+  fi
+
+  LENGTH=$(grep -i content-length <<< "$HEADERS" | awk '{print $2}' | tr -d '\r')
+  if [ "$LENGTH" -lt "$MINLENGTH" ] ; then
+    echo Content length too small: $LENGTH
+    exit 1
+  fi
+}
+
+echo Building $DOCKER_IMAGE
+docker build --tag=$DOCKER_IMAGE -f Dockerfile .
+
+echo Running $DOCKER_IMAGE
+docker run -d --rm -p 8080:8080 --name hsl-map-server $DOCKER_IMAGE
+sleep 30
+
+test http://localhost:8080/map/v1/hsl-map/14/9326/4739.png 50000
+test http://localhost:8080/map/v1/hsl-stop-map/14/9326/4739.pbf 1000
+
+echo Stopping $DOCKER_IMAGE
+docker stop hsl-map-server
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   docker login -u $DOCKER_USER -p $DOCKER_AUTH
