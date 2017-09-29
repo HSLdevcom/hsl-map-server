56a62b6d3b9a91fc87a1d7af4378139e2684bce9
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-09-29 12:20:15 +0300

    Add more tests to travis build

--- travis-build.sh
+++ travis-build.sh
@@ -45,8 +45,17 @@ echo Running $DOCKER_IMAGE
 docker run --rm -p 8080:8080 -h hsl-map-server --name hsl-map-server -e FONTSTACK_PASSWORD=$FONTSTACK_PASSWORD $DOCKER_IMAGE &
 sleep 60
 
-test http://localhost:8080/map/v1/hsl-map/14/9326/4739.png 50000
-test http://localhost:8080/map/v1/hsl-stop-map/14/9326/4739.pbf 1000
+test http://localhost:8080/map/v1/hsl-map/14/9326/4739.png 80000
+test http://localhost:8080/map/v1/hsl-map-sv/14/9326/4739.png 80000
+test http://localhost:8080/map/v1/hsl-map-fi-sv/14/9326/4739.png 80000
+
+test http://localhost:8080/map/v1/hsl-stop-map/14/9326/4739.pbf 2000
+test http://localhost:8080/map/v1/finland-stop-map/14/9326/4739.pbf 2000
+test http://localhost:8080/map/v1/waltti-stop-map/14/9363/4546.pbf 2000
+
+test http://localhost:8080/map/v1/hsl-citybike-map/14/9326/4739.pbf 40
+test http://localhost:8080/map/v1/hsl-parkandride-map/14/9326/4739.pbf 500
+test http://localhost:8080/map/v1/hsl-ticket-sales-map/14/9326/4739.pbf 500
 
 echo Stopping $DOCKER_IMAGE
 docker stop hsl-map-server
