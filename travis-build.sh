a23af5a7b0daa9e309666ade2d25390912f11d50
Antti Silventoinen <antti.silventoinen@iki.fi>
2017-09-29 15:33:26 +0300

    Improve test logs

--- travis-build.sh
+++ travis-build.sh
@@ -16,26 +16,28 @@ function test {
   URL=$1
   MINLENGTH=$2
 
-  echo Testing $URL
+  echo $URL - Testing
 
   HEADERS=$(curl -sI $URL)
 
   if [ -z "$HEADERS" ] ; then
-    echo No response
+    echo $URL - No response
     exit 1
   fi
 
   STATUS=$(head -1 <<< "$HEADERS")
   if ! grep -q "200 OK" <<< "$HEADERS" ; then
-    echo Unexpected status code: $STATUS
+    echo $URL - Unexpected status code: $STATUS
     exit 1
   fi
 
   LENGTH=$(grep -i content-length <<< "$HEADERS" | awk '{print $2}' | tr -d '\r')
   if [ "$LENGTH" -lt "$MINLENGTH" ] ; then
-    echo Content length too small: $LENGTH
+    echo $URL - Content length too small: $LENGTH
     exit 1
   fi
+
+  echo $URL - OK
 }
 
 echo Building $DOCKER_IMAGE
