43be0605e7d3c93514f7e6f5c3d442f1dbce4939
TeemuP <teemu.peltonen@cgi.com>
2018-05-31 10:53:59 +0300

    Update packages & enable multiprocess

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ cd ${WORK} && \
 Xorg -dpi 96 -nolisten tcp -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./10.log -config ./xorg.conf :10 & \
 sleep 15 && \
 DISPLAY=":10" node_modules/.bin/forever start -c "node ${NODE_OPTS}" \
-node_modules/tessera/bin/tessera.js --port 8080 --config config.js \
+node_modules/tessera/bin/tessera.js --port 8080 --config config.js --multiprocess \
 -r ${WORK}/node_modules/tilelive-otp-citybikes/ \
 -r ${WORK}/node_modules/tilelive-otp-stops/ \
 -r ${WORK}/node_modules/tilelive-otp-routes/ \
