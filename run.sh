a1de0f6e99a09d31133609c9f1c4f155f942a45d
Joakim Kronqvist <joakim.kronqvist@netlight.com>
2018-08-13 14:20:45 +0300

    Add newline so Azure DNS fix wont mess up the file

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #workaround for azure DNS issue
 
 if [ "$EUID" -eq 0 ]
-  then echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
+  then echo -e "\nsearch marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf
 fi
 
 #start map server
