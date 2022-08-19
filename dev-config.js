490832db67f9b0d28b18da09c837e7824a9fb2dc
Juho Hänninen <juho.hanninen@cgi.com>
2022-08-19 18:42:54 +0300

    Upgrade tilelive-geojson to working version

--- dev-config.js
+++ dev-config.js
@@ -1,9 +1,16 @@
 const sourcesUrl = "http://localhost:8080/";
 
 module.exports = {
+  "/map/v1/hsl-ticket-sales-map": {
+    "source": "hslticketsales://",
+    "headers": {
+      "Cache-Control": "public,max-age=604800"
+    }
+  },
   "/map/v2/hsl-ticket-sales-map": {
     "source": {
       "protocol": "geojson:",
+      "query": {},
       "name": "Ticket sales",
       "maxzoom": 20,
       "sources": [{
