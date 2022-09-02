d7f524fa1e1aac40a64cf23c9de898a48c3a9263
Juho Hänninen <juho.hanninen@cgi.com>
2022-09-02 11:03:57 +0300

    Change parkandride layer to the new module

--- data-fetcher/configurations.js
+++ data-fetcher/configurations.js
@@ -1,9 +1,25 @@
 const {
+  PARKANDRIDE_URL,
   TICKET_SALES_URL
 } = require("../constants");
 const { queries, wranglers } = require("./data");
 
 const layers = [
+  {
+    name: "hsl-parkandride",
+    sources: [
+      {
+        url: `${PARKANDRIDE_URL}facilities.geojson`,
+        wrangler: wranglers.dummyGeojsonWrangler,
+        file: "hsl-parkandride-facilities.geojson",
+      },
+      {
+        url: `${PARKANDRIDE_URL}hubs.geojson`,
+        wrangler: wranglers.dummyGeojsonWrangler,
+        file: "hsl-parkandride-hubs.geojson",
+      }
+    ]
+  },
   {
     name: "hsl-ticket-sales",
     sources: [
--- dev-config.js
+++ dev-config.js
@@ -1,12 +1,6 @@
 const sourcesUrl = "http://localhost:8080/";
 
 module.exports = {
-  "/map/v1/hsl-ticket-sales-map": {
-    "source": "hslticketsales://",
-    "headers": {
-      "Cache-Control": "public,max-age=604800"
-    }
-  },
   "/map/v2/hsl-ticket-sales-map": {
     "source": {
       "protocol": "geojson:",
@@ -16,11 +10,33 @@ module.exports = {
       "sources": [{
         "id": "ticket-sales",
         "description": "",
-        "file": "/opt/hsl-map-server/ticket-sales.geojson",
+        "file": "/opt/hsl-map-server/hsl-ticket-sales.geojson",
+      }]
+    },
+    "headers": {
+      "Cache-Control": "public,max-age=604800"
+    }
+  },
+  "/map/v2/hsl-parkandride": {
+    "source": {
+      "protocol": "geojson:",
+      "query": {},
+      "name": "HSL Park & Ride",
+      "maxzoom": 20,
+      "bounds": [18, 58, 32, 71],
+      "center": [24.9, 60.1, 14],
+      "sources": [{
+        "id": "facilities",
+        "description": "",
+        "file": "/opt/hsl-map-server/hsl-parkandride-facilities.geojson",
+      }, {
+        "id": "hubs",
+        "description": "",
+        "file": "/opt/hsl-map-server/hsl-parkandride-hubs.geojson",
       }]
     },
     "headers": {
       "Cache-Control": "public,max-age=604800"
     }
   }
-}
+};
